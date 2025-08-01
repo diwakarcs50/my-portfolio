39d7d35e6bb433e08e09cf5f11532c3cad81b45a
diwakar <d.7484959595@gmail.com>
2025-08-01 12:26:46 +0530

    fixed lint

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
     <div className="divide-y divide-gray-100 dark:divide-gray-700">
       <div className="space-y-2 pt-5 pb-8 md:space-x-5">
         <h1
-          className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl
-        sm:leading-10 md:text-6xl md:leading-13 animate-fade-in"
+          className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-13 animate-fade-in"
         >
           Home
         </h1>
